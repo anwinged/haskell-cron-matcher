44dd3aad0a6117a725d95a57b7ad1586ee7fa86f
Anton Vakhrushev <anwinged@ya.ru>
2017-11-11 10:25:47 +0300

    Match all asterisk pattern

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,5 +5,25 @@ module Lib
 import Data.Bool
 import Data.Dates
 
+data Range = Any | Pair Int Int | Sequence [Int]
+
+data Step = All | Value Int
+
+data CronItemPattern = CronItemPattern Range Step
+
+data CronPattern = CronPattern [CronItemPattern]
+
 check :: DateTime -> String -> Bool
-check _ _ = False
+check d s = checkPattern d (parseCronPattern s)
+
+parseCronPattern :: String -> CronPattern
+parseCronPattern s = CronPattern $ map parseCronItemPattern $ words s
+
+parseCronItemPattern :: String -> CronItemPattern
+parseCronItemPattern "*" = CronItemPattern Any All
+
+checkPattern :: DateTime -> CronPattern -> Bool
+checkPattern d (CronPattern (x:xs)) = checkItemPattern (year d) x
+
+checkItemPattern :: Int -> CronItemPattern -> Bool
+checkItemPattern n (CronItemPattern Any All) = True
